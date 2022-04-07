36ef306e77480be03d415099935d70a87275caaa
ibondarovich <ilona.bondarovich@gmail.com>
2022-04-07 19:42:44 +0300

    add files from laba1

---
+++ Figures/Ellipse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Ellipse:Point
+    {
+        private double semi_axel1;
+        private double semi_axel2;
+
+        public Ellipse(double x, double y, double semi_axel1, double semi_axel2) :base(x,y)
+        {
+            Semi_axel1 = semi_axel1;
+            Semi_axel2 = semi_axel2;
+        }
+
+        public double Semi_axel1
+        {
+            get{
+                return semi_axel1;
+            }
+
+            set{
+                semi_axel1 = value;
+            }
+        }
+        public double Semi_axel2
+        {
+            get{
+                return semi_axel2;
+            }
+
+            set{
+                semi_axel2 = value;
+            }
+        }
+
+        public override void ShowFigure()
+        {
+            Console.WriteLine("Эллипс с центром в точке ({0};{1} и полуосями {2} и {3})",X,Y,Semi_axel1,Semi_axel2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FigureList figureList = new();
+            figureList.AddList(new Point(20, 30));
+            figureList.AddList(new Line(20, 30, 50, 10));
+            figureList.AddList(new Ellipse(0,0,5,3));
+            figureList.AddList(new Circle(6,10,20));
+            figureList.AddList(new Rectangle(3,4,20,100));
+            figureList.AddList(new Triangle(10,20,30,40,50,60));
+
+    
+
+            figureList.DrawFigures();
+            
+        }
+    }
+   
+}
